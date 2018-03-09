16a4b0ce01fd52e84ea83cd41ae8fff2f1ae3bd2
Fathy Boundjadj <fathy.boundjadj@gmail.com>
2018-03-10 00:02:31 +0100

    Ignore require if it is defined

---
+++ test/integration/require-scope/index.js
@@ -0,0 +1,37 @@
+const test = {
+  unaryFnExpr: 'test failed',
+  fnExpr: 'test failed',
+  fnDecl: 'test failed',
+  varDecl: 'test failed',
+  // TODO topVarDecl: 'test failed'
+};
+
+module.exports.test = test;
+
+function main(require) {
+  require('test passed');
+}
+
+main(x => test.fnDecl = x);
+
+(function(require) {
+  require('test passed')
+})(x => test.fnExpr = x);
+
+void function main(require) {
+  require('test passed');
+}(x => test.unaryFnExpr = x);
+
+void function main() {
+  const require = x => test.varDecl = x
+
+  require('test passed')
+}()
+
+/* TODO
+function require(x) {
+  return test.topVarDecl = x;
+}
+
+require('test passed')
+*/
